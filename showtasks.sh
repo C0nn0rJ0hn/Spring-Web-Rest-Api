f86e8b696b01643891a801d3f05cf81e18c46f0f
Paweł Piętoń <pawel.pieton@gmail.com>
2021-05-28 18:27:01 +0200

    21.4 Added runcrud.sh and showtasks.sh

---
+++ showtasks.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+export CATALINA_HOME=/Users/geoprojekt3d/Desktop/Development/Projects/tasks
+
+start_crud() {
+  $CATALINA_HOME/runcrud.sh start
+}
+
+fail() {
+   echo "Something went wrong"
+}
+
+end() {
+   echo "Browser was open correctly"
+}
+
+open_safari() {
+  open -a safari 'http://localhost:8080/crud/v1/task/getTasks'
+}
+
+if start_crud; then
+  open_safari
+  end
+else
+  fail
+fi
+
