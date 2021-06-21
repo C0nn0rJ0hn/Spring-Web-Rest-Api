97afa498d030d8a31430c881546db1e74cf6765d
Paweł Piętoń <pawel.pieton@gmail.com>
2021-06-21 15:41:09 +0200

    22.4 exercise

--- src/test/java/com/crud/tasks/trello/client/TrelloClientTest.java
+++ src/test/java/com/crud/tasks/trello/client/TrelloClientTest.java
@@ -89,4 +89,26 @@ class TrelloClientTest {
         Assertions.assertEquals("Test task", newCard.getName());
         Assertions.assertEquals("http://test.com", newCard.getShortUrl());
     }
+
+    @Test
+    public void shouldReturnEmptyList() throws URISyntaxException {
+        //Given
+        when(trelloConfig.getTrelloApiEndpoint()).thenReturn("http://test.com");
+        when(trelloConfig.getTrelloAppKey()).thenReturn("test");
+        when(trelloConfig.getTrelloToken()).thenReturn("test");
+        when(trelloConfig.getTrelloUsername()).thenReturn("test");
+
+        TrelloBoardDto[] trelloBoardDto = new TrelloBoardDto[1];
+        trelloBoardDto[0] = new TrelloBoardDto("test_id", "test_board", new ArrayList<>());
+
+        URI uri = new URI("http://test.com/members/test/boards?key=test&token=test&fields=name,id&lists=all");
+
+        when(restTemplate.getForObject(uri, TrelloBoardDto[].class)).thenReturn(null);
+
+        //When
+        List<TrelloBoardDto> emptyListTrelloBoard = trelloClient.getTrelloBoards();
+
+        //Then
+        Assertions.assertEquals(0, emptyListTrelloBoard.size());
+    }
 }
